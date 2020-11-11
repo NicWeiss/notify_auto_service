ff278fd0a71a4400ed5f53c5a81e5ca5f79a01a9
NicWeiss <ixornic@gmail.com>
2020-11-11 21:39:57 +0300

    fix сериалайзеря для получателей

--- frontend/app/models/acceptor.js
+++ frontend/app/models/acceptor.js
@@ -1,8 +1,8 @@
-import Model, { attr } from '@ember-data/model'; 
+import Model, { attr } from '@ember-data/model';
 
 export default class AcceptorAddModel extends Model {
   @attr('string') name
-  @attr('string') system_id
+  @attr('string') systemId
   @attr('string') account
   @attr('string') status
   @attr('string') type
