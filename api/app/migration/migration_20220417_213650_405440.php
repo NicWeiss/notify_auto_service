22720198b3e3a170f8f804ab29851c303f44e738
NicWeiss <ixornic@gmail.com>
2022-04-18 01:18:59 +0300

    миграция данных

---
+++ api/app/migration/migration_20220417_213650_405440.php
@@ -0,0 +1,47 @@
+<?php
+/*
+*   migration 20220417_213650_405440
+*/
+
+namespace migration;
+
+use generic\migration;
+use lib\dba;
+
+
+final class migration_20220417_213650_405440 extends migration
+{
+
+    protected $comment = 'Convert data into new format';
+
+    protected function up()
+    {
+        $notify = 'notifier.notify';
+
+        $sql = "SELECT * FROM $notify  WHERE date != '' ;";
+        dba::query($sql);
+        $notify_list = dba::fetch_assoc_all();
+
+        foreach ($notify_list as $key => $value) {
+            if (preg_match('/[0-9]{2}\.[0-9]{2}\.[0-9]{4}/', $value['date'])) {
+                $notify_id = $value['id'];
+                $date = explode('.', $value['date']);
+                $new_date = $date[2] . '-' . $date[1] . '-' . $date[0];
+
+                $sql = "UPDATE $notify SET `date`= '$new_date' WHERE `id`='$notify_id'";
+                dba::query($sql);
+            }
+        }
+
+        return true;
+    }
+
+    protected function down()
+    {
+        $query = "";
+        if (!dba::query($query))
+            return false;
+
+        return true;
+    }
+}
