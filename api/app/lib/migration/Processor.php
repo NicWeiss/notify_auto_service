ce814a24b8c951af88a348471ee9539c5d37557a
NicWeiss <ixornic@gmail.com>
2022-08-30 21:18:42 +0300

    Правка тестирования

--- api/app/lib/migration/Processor.php
+++ api/app/lib/migration/Processor.php
@@ -18,7 +18,7 @@ class Processor
 
         if (gettype($db_migrations) != 'array') {
             print("Таблица миграций не инициализирована! \n");
-            exit(1);
+            exit(127);
         }
 
         $result = [];
